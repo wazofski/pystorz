d8ffd4a53433b25080c9616bfb114e4267aee293
wazofski <wazo@duck.com>
2023-04-24 07:18:52 -0700

    more packaging

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/wazofski/pystorz',
-    packages=find_packages(include=["pystorz/*"]),
+    packages=["pystorz"],
     # package_dir={'': 'src'},
     # include_package_data=True,
     install_requires=[
