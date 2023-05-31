383a719dc37b8bdc91b9eff7680c5836552862d9
wazofski <wazo@duck.com>
2023-05-30 19:10:49 -0700

    formatting

--- test.py
+++ test.py
@@ -6,7 +6,8 @@ from pystorz.mgen.test import test_mgen
 import logging, logging.config
 
 import yaml
-with open('config/logger.yml', 'r') as f:
+
+with open("config/logger.yml", "r") as f:
     logging.config.dictConfig(yaml.safe_load(f.read()))
 
 log = logging.getLogger(__name__)
@@ -14,13 +15,15 @@ log = logging.getLogger(__name__)
 
 def test_mgen_can_generate():
     err = Generate("tests/model")
-    
+
     assert err is None
+
 
 log.debug("starting test_mgen_can_generate")
 
 test_mgen_can_generate()
 test_mgen()
+
 
 def test_sqlite_store():
     from pystorz.sql.store import SqliteStore, SqliteConnection
@@ -34,5 +37,6 @@ def test_sqlite_store():
     from tests.store import common_test_suite
 
     common_test_suite(clt)
+
 
 test_sqlite_store()
